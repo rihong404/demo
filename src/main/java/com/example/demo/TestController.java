7ed3027ac9b4270917ce473d65eb88fa3b262cab
Lenovo <rihong404@qq.com>
2022-09-03 01:12:48 +0800

    demo测试2

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -4,5 +4,9 @@ public class TestController {
 
     public void test(){
         System.out.println("12323");
+        System.out.println("434");
+        System.out.println("4234");
+        System.out.println("77");
+
     }
 }
